0a874466ce086409a497e5b9b3a3bbf9b8c02ef7
Murasakibara <joackimcoulibaly5@gmail.com>
2022-09-08 11:51:05 +0000

    dd

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,6 @@ class AuthController extends Controller
     {
             $validatedData = $request->validate([
             'nom' => 'required|string|max:100',
-                'prenom' => 'required|string|max:255',
                 'contact' => 'required|string|max:15',
                 'role' => 'required|string|max:15',
                             'email' => 'required|string|email|max:255|unique:users',
@@ -22,7 +21,6 @@ class AuthController extends Controller
 
             $user = User::create([
                     'nom' => $validatedData['nom'],
-                    'prenom' => $validatedData['prenom'],
                     'contact' => $validatedData['contact'],
                     'role' => $validatedData['role'],
                         'email' => $validatedData['email'],
