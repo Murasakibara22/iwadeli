695b6b12969eb3a4b290cf802ab4be874769f639
Murasakibara <joackimcoulibaly5@gmail.com>
2022-11-15 13:33:59 +0000

    mise en place du logo

--- resources/views/dash/partials/navbar.blade.php
+++ resources/views/dash/partials/navbar.blade.php
@@ -295,20 +295,20 @@
     <!-- Logo Light -->
     <a href="/dashboard" class="logo logo-light">
         <span class="logo-lg">
-            <img src="assets/images/logo/Jiam_s2-removebg-preview.png" alt="logo" height="130">
+            <img src="../logo/Jiam_s2-removebg-preview.png" alt="logo" height="130">
         </span>
         <span class="logo-sm">
-            <img src="assets/images/logo/Jiam_s4-removebg-preview.png" alt="small logo" height="50">
+            <img src="../logo/Jiam_s4-removebg-preview.png" alt="small logo" height="50">
         </span>
     </a>
 
     <!-- Logo Dark -->
     <a href="/dashboard" class="logo logo-dark">
         <span class="logo-lg">
-            <img src="assets/images/logo/Jiam_s3-removebg-preview.png" alt="dark logo" height="60">
+            <img src="../logo/Jiam_s3-removebg-preview.png" alt="dark logo" height="60">
         </span>
         <span class="logo-sm">
-            <img src="assets/images/logo/Jiam_s5-removebg-preview.png" alt="small logo" height="60">
+            <img src="../logo/Jiam_s5-removebg-preview.png" alt="small logo" height="60">
         </span>
     </a>
 
