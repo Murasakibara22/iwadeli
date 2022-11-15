38325742c89f4f01d387fef0d88ed906af51fdfb
Murasakibara <joackimcoulibaly5@gmail.com>
2022-11-15 13:11:30 +0000

    mise en place du formulaire d'inscription et de connexion

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,71 +1,75 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('register') }}">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Troc moi</title>
+    <link rel="stylesheet" href="{{ asset('FormStyle/style.css') }}">
+</head>
+<body>
+<h2></h2>
+<div class="container" id="container">
+	<div class="form-container sign-up-container">
+     <form method="POST" action="{{ route('register') }}">
             @csrf
+            @method('POST')
+			<h1>Inscrivez-vous</h1>
+			
+			<span></span>
+			<input type="text" placeholder="Votre Nom" name="nom" />
+			<input type="text" placeholder="Votre prenom" name="prenom" />
+			<input type="text" placeholder="Votre Contact" name="contact" value="+225"/>
+			<input type="email" placeholder="Email" name="email" />
+			<input type="password" placeholder="Password" name="password" />
+			<input type="password" placeholder=" Confirm Password" name="password_confirmation" />
+			<button type="submit">Valider</button>
+		</form>
+	</div>
+	<div class="form-container sign-in-container" >
+     <form method="POST" action="{{ route('login') }}">
+            @csrf
+			<h1>Login</h1>
+			<div class="social-container">
+				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+			</div>
+			<span>Connecter vous ici</span>
+			<input type="email" placeholder="Email" name="email" />
+			<input type="password" placeholder="Mot de passe" name="password"  />
+			<a href="#">Mot de passe Oublier ?</a>
+			<button>Connexion</button>
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-left">
+				<h1>Bon retour</h1>
+				<p>Rester connecter avec nous en cliquant ici et en entrant vos informations personnels</p>
+				<button class="ghost" id="signIn">Connexion</button>
+			</div>
+			<div class="overlay-panel overlay-right">
+				<h1>Salut, Friend!</h1>
+				<p>Entrer vos informations et connectez vous facilement</p>
+				<button class="ghost" id="signUp">Inscription</button>
+			</div>
+		</div>
+	</div>
+</div>
 
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
+<footer>
+	<p>
+		Created with <i class="fa fa-heart"></i> by
+		<a target="_blank" href="https://florin-pop.com">Florin Pop</a>
+		- Read how I created this and how you can join the challenge
+		<a target="_blank" href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/">here</a>.
+	</p>
+</footer>
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('prenom')" />
 
-                <x-input id="prenom" class="block mt-1 w-full" type="text" name="prenom" :value="old('prenom')" required />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="email" :value="__('contact')" />
-
-                <x-input id="contact" class="block mt-1 w-full" type="text" name="contact" :value="old('contact')" required />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+<script src="{{ asset('FormStyle/main.js') }}"></script>
+</body>
+</html>
