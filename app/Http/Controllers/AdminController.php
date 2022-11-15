24ee274690d53a97475029755cdb09e1a8cd790c
Murasakibara <joackimcoulibaly5@gmail.com>
2022-11-15 11:55:25 +0000

    mise en place de la vue dash

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\dash;
+namespace App\Http\Controllers;
 
 use App\Models\Pays;
 use App\Models\User;
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\dash\AdminController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
