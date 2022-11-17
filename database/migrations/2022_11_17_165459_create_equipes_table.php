5d205315b76e0e79c8a887363ac39ff6a1b04d12
Murasakibara <joackimcoulibaly5@gmail.com>
2022-11-17 16:58:13 +0000

    ajout de la table membre

---
+++ database/migrations/2022_11_17_165459_create_equipes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEquipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('contact');
+            $table->string('email');
+            $table->string('photo');
+            $table->string('fonction');
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipes');
+    }
+}
