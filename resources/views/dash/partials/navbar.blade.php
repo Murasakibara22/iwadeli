1ad61de8a53b225c5477ff5fd6be3409da8f0e99
Murasakibara <joackimcoulibaly5@gmail.com>
2022-11-18 03:31:40 +0000

    mise en place du logo

--- resources/views/dash/partials/navbar.blade.php
+++ resources/views/dash/partials/navbar.blade.php
@@ -10,20 +10,20 @@
             <!-- Logo light -->
             <a href="/dashboard" class="logo-light">
                 <span class="logo-lg">
-                    <img src="../logo/Jiam_s2-removebg-preview.png" alt="logo" height="22">
+                    <img src="../logo/logo.png" alt="logo" height="22">
                 </span>
                 <span class="logo-sm">
-                    <img src="../logo/Jiam_s2-removebg-preview.png" alt="small logo" height="22">
+                    <img src="../logo/logo.png" alt="small logo" height="22">
                 </span>
             </a>
 
             <!-- Logo Dark -->
             <a href="/dashboard" class="logo-dark">
                 <span class="logo-lg">
-                    <img src="../logo/Jiam_s5-removebg-preview.png" alt="dark logo" height="22">
+                    <img src="../logo/logo.png" alt="dark logo" height="22">
                 </span>
                 <span class="logo-sm">
-                    <img src="../logo/Jiam_s5-removebg-preview.png" alt="small logo" height="60">
+                    <img src="../logo/logo.png" alt="small logo" height="60">
                 </span>
             </a>
         </div>
@@ -295,20 +295,20 @@
     <!-- Logo Light -->
     <a href="/dashboard" class="logo logo-light">
         <span class="logo-lg">
-            <img src="../logo/Jiam_s2-removebg-preview.png" alt="logo" height="130">
+            <img src="../logo/logo.png" alt="logo" height="60">
         </span>
         <span class="logo-sm">
-            <img src="../logo/Jiam_s4-removebg-preview.png" alt="small logo" height="60">
+            <img src="../logo/logo.png" alt="small logo" height="60">
         </span>
     </a>
 
     <!-- Logo Dark -->
     <a href="/dashboard" class="logo logo-dark">
         <span class="logo-lg">
-            <img src="../logo/Jiam_s5-removebg-preview.png" alt="dark logo" height="60">
+            <img src="../logo/logo.png" alt="dark logo" height="60">
         </span>
         <span class="logo-sm">
-            <img src="../logo/Jiam_s5-removebg-preview.png" alt="small logo" height="60">
+            <img src="../logo/logo.png" alt="small logo" height="60">
         </span>
     </a>
 
