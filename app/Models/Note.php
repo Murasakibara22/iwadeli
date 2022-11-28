b50badb16341326f577881d25d042db54f52e178
Murasakibara <joackimcoulibaly5@gmail.com>
2022-11-28 18:49:53 +0000

    mise en place du model Note

---
+++ app/Models/Note.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Livreur;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Note extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [ 
+        'nbEtoile',
+        'details',
+        'id_livreurs'
+    ];
+
+    public function notes(){
+        $this->belongsTo(Livreur::class, 'id_livreurs');
+    }
+}
---
+++ database/migrations/2022_11_28_184427_create_notes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('nbEtoile');
+            $table->string('details')->nullable();
+            $table->timestamps();
+
+            $table->unsignedBigInteger('id_livreurs');
+            $table->foreign('id_livreurs')->on('livreurs')->references('id')->onDelete('cascade'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
