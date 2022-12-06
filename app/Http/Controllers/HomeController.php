de97709cb2611cb183bfb203bbc038654a8e71e1
Murasakibara <joackimcoulibaly5@gmail.com>
2022-12-06 13:30:59 +0000

    mise en place de l'espace Temoignages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Temoignage;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
+
         return view('welcome');
     }
 
